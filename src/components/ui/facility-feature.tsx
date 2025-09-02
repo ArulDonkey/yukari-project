ed66b661d58ba503cce955da35234f7f611835f8
ArulDonkey <pahaerulm@gmail.com>
2025-09-02 21:46:11 +0700

    Fixing UI HomePage

---
+++ src/components/ui/facility-feature.tsx
@@ -0,0 +1,44 @@
+import { Separator } from "@/components/ui/separator";
+import type { LucideIcon } from "lucide-react";
+
+type Components = {
+  icon: LucideIcon;
+  title: string;
+};
+
+interface ComponentsProps {
+  items: Components[];
+}
+
+export function Components({ items }: ComponentsProps) {
+  return (
+    <section className="bg-[#AB0103] text-white py-16 lg:py-30">
+      <div className="max-w-5xl mx-auto grid grid-cols-2 grid-rows-2 relative">
+        {/* Garis tengah */}
+        <Separator
+          orientation="vertical"
+          className="absolute inset-y-0 left-1/2 bg-white w-[2px] hidden md:block"
+        />
+        <Separator
+          orientation="horizontal"
+          className="absolute inset-x-0 top-1/2 bg-white h-[2px] hidden md:block"
+        />
+
+        {items.map((item, i) => {
+          const Icon = item.icon;
+          return (
+            <div
+              key={i}
+              className="flex flex-col items-center justify-center py-12 text-center"
+            >
+              <div className="mb-4">
+                <Icon className="w-20 h-20 text-white" />
+              </div>
+              <p className="text-2xl">{item.title}</p>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+}
